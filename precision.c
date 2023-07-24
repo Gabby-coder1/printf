df531bc545c2a8e873f8c60eb051dc9136de365e
PatsoyaKgotso <133250237+PatsoyaKgotso@users.noreply.github.com>
2023-07-24 13:23:14 +0200

    Update precision.c

--- precision.c
+++ precision.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * get_precision - Calculates the precision for printing
- * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed.
+ * get_precision - Calculates precision for printing
+ * @format: Formatted string which to print arguments
+ * @i: Arguments to be printed.
  * @list: list of arguments.
  *
  * Return: Precision.
