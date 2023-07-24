5d7aa6eff25361b07d8eba680071bf288fabd71e
PatsoyaKgotso <133250237+PatsoyaKgotso@users.noreply.github.com>
2023-07-24 13:12:21 +0200

    Update get_size.c

--- get_size.c
+++ get_size.c
@@ -2,8 +2,8 @@
 
 /**
  * get_size - Calculates the size to cast the argument
- * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed.
+ * @format: Formatted string which to print arguments
+ * @i: Arguments to be printed.
  *
  * Return: Precision.
  */
